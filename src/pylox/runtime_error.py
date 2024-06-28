31d4021be2e6175aeeeef705e1840b3dde05f7db
Giulio De Matteis <hello@giuliodematte.is>
2024-06-28 15:38:03 +0200

    Add message to LoxRuntimeError

--- src/pylox/runtime_error.py
+++ src/pylox/runtime_error.py
@@ -5,3 +5,4 @@ class LoxRuntimeError(RuntimeError):
     def __init__(self, token: Token, message: str):
         super().__init__(message)
         self.token = token
+        self.message = message
